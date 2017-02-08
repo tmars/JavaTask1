356e057fa18786717ebb83341a387ac292afb53b
Marsel <t.mars@mail.ru>
2017-02-08 16:46:26 +0300

    Написан обработчик ResourceReader, который читает ресурс с помощью Parser и передает значения в Totalizer

---
+++ src/com/talipov/ResourceReader.java
@@ -0,0 +1,65 @@
+package com.talipov;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by Марсель on 08.02.2017.
+ */
+public class ResourceReader {
+
+    private Parser parser;
+    private Totalizer totalizer;
+
+    public ResourceReader(String resource, Totalizer totalizer) throws FileNotFoundException {
+        InputStream stream = getStream(resource);
+        if (stream == null) {
+            throw new FileNotFoundException();
+        }
+
+        this.parser = new Parser(new Scanner(stream));
+        this.totalizer = totalizer;
+    }
+
+    public void read() {
+        Integer val = null;
+        try {
+            while ((val = parser.getNext()) != null) {
+                totalizer.add(val);
+            }
+        } catch (ParserErrorException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private InputStream getStream(String path) {
+        URL url;
+        if (path.startsWith("http://") || path.startsWith("https://")) {
+            try {
+                url = new URL(path);
+            } catch (MalformedURLException e) {
+                System.out.println("Ошибка чтения ресурса по URL:" + path);
+                return null;
+            }
+        } else {
+            url = getClass().getResource(path);
+            if (url == null) {
+                System.out.println("Ошибка чтения файла ресурса:" + path);
+                return null;
+            }
+        }
+
+        try {
+            return url.openStream();
+        } catch (IOException e) {
+            System.out.println("Ошибка при работе с ресурсом:" + path);
+        }
+
+        return null;
+    }
+}
